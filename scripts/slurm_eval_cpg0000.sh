cbb00f2ed0decdd99eb5cdb5be02eaaa3c862b5b
Adnan Karim <skiwordylectures@gmail.com>
2026-03-11 13:53:49 +0100

    added all

---
+++ scripts/slurm_eval_cpg0000.sh
@@ -0,0 +1,29 @@
+python submitit_train.py \
+    --partition=pasteur \
+    --account=pasteur \
+    --exclude=pasteur1,pasteur2,pasteur3,pasteur4 \
+    --constraint "" \
+    --dataset=cpg0000 \
+    --config=cpg0000 \
+    --batch_size=32 \
+    --accum_iter=1 \
+    --eval_frequency=10 \
+    --epochs=3000 \
+    --class_drop_prob=0.2 \
+    --cfg_scale=0.2 \
+    --compute_fid \
+    --ode_method heun2 \
+    --ode_options '{"nfe": 50}' \
+    --use_ema \
+    --edm_schedule \
+    --skewed_timesteps \
+    --fid_samples=30720 \
+    --job_dir=/path/to/your/outputs/ \
+    --shared_dir=/path/to/your/shared/ \
+    --use_initial=2 \
+    --eval_only \
+    --noise_level=1.0 \
+    --save_fid_samples \
+    --resume=/path/to/your/checkpoint.pth \
+    --start_epoch=your_start_epoch \
+    --ngpus=4 \
